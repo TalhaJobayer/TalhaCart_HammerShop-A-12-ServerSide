62a933b404c9ea9dde6cf39d74fa74dab8527ef4
Talha Jobayer <talhajobayer@gmail.com>
2022-05-23 01:37:19 +0600

    initial settings done and api loaded done

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port =process.env.PORT || 5000
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleWare=====
+app.use(cors())
+app.use(express.json())
+// middleWare=====
+
+
+// =======================================
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.z4bsa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+      await client.connect();
+      const HammerCollection = client.db("hammerCollection").collection("hammer");
+      app.get('/products', async (req,res)=>{
+       
+     
+        // query for movies that have a runtime less than 15 minutes
+        const query = {  };
+       
+        const cursor =  HammerCollection.find(query);
+        // print a message if no documents were found
+       
+        // replace console.dir with your callback to access individual elements
+        const result=await cursor.toArray();
+        res.send(result)
+      })
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+// =======================================
+
+app.get('/', (req, res) => {
+  res.send('ohh talha well done.doing great.keep it up')
+})
+
+app.listen(port, () => {
+  console.log(`server is on the way.and so are you ${port}`)
+})
